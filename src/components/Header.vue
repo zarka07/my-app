05828808fff6ac3235b8cd59d4afd9013500cbb4
Serhii <tarasov.serhii@it-serve.net>
2021-11-25 17:57:22 +0200

    working localization

---
+++ src/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+    <ul type="none">
+        <li class="changeLang" v-for="locale in locales" :key="locale" @click="changeLang(locale)" > 
+            {{$t("AppVue.Language")}}: {{locale}}
+        </li>
+    </ul>
+    
+    </div>
+</template>
+
+<script>
+
+export default({
+    name:'Header',
+    methods:{
+        changeLang(locale){
+            if(this.$i18n.locale!== locale){
+                this.$i18n.locale = locale;
+            }
+        }
+    },
+    data(){
+        return {
+            locales: process.env.VUE_APP_I18N_SUPPORTED_LOCALE.split(',')
+        }
+    },
+    
+    
+})
+</script>
+<style scoped>
+    .changeLang{
+        border:1px solid;
+        color:blue;
+        width:100px;
+        margin:5px;
+        text-decoration: none;
+        color: #459CE7;
+        cursor: pointer;
+    }
+</style>
